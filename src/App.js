3d7a87864ed101b9c0f3e8d243fb9a9bebad7880
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-07-05 17:11:13 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Dashboard from "./pages/Dashboard";
+import CreateUser from "./pages/CreateUser";
+import UpdateUser from "./pages/UpdateUser";
+import SingleUser from "./pages/SingleUser";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path={'/dashboard'} element={<Dashboard/>}/>
+        <Route path={'/create'} element={<CreateUser/>}/>
+        <Route path={'/update/:id'} element={<UpdateUser/>}/>
+        <Route path={'/user/:id'} element={<SingleUser/>}/>
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
     </div>
   );
 }
