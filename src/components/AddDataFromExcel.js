4180d1d2d29ad708a7fb929d42f7c3591a3ec76d
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-07-06 14:00:02 +0530

    upload component styles added

--- src/components/AddDataFromExcel.js
+++ src/components/AddDataFromExcel.js
@@ -1,31 +1,48 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { Button } from "antd";
+import { Button,message } from "antd";
 import axios from "axios";
+
+import '../styles/AddDataFromExcel.css'
 
 const AddDataFromExcel = () => {
     const navigate = useNavigate();
     const [selectedFile, setSelectedFile] = useState(null);
+    const [messageApi, contextHolder] = message.useMessage();
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
+    const handleFileChange = async (event) => {
+        const selectedFile = event.target.files[0];
+        const allowedExtensions = ['.xlsx', '.xls'];
+        const fileExtension = selectedFile.name.split('.').pop();
+        const isExcelFile = allowedExtensions.includes(`.${fileExtension.toLowerCase()}`);
+        const maxSizeInBytes = 2 * 1024 * 1024;
+
+        if(isExcelFile && selectedFile.size < maxSizeInBytes){
+            setSelectedFile(selectedFile);
+        }else{
+            messageApi.warning('Select valid excel file', 10000);
+        }
     };
 
     const handleFileUpload = async () => {
-        try{
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-            await axios.post(`http://localhost:8080/upload`, formData);
-            navigate('/dashboard')
-        }catch (err){
-            console.error(err);
+        if(selectedFile !== null){
+            try{
+                const formData = new FormData();
+                formData.append('file', selectedFile);
+                await axios.post(`http://localhost:8080/upload`, formData);
+                navigate('/dashboard')
+            }catch (err){
+                console.error(err);
+            }
         }
     };
 
     return(
-        <div>
-            <input type="file" onChange={handleFileChange} />
-            <Button onClick={handleFileUpload}>Upload</Button>
+        <div className="file-upload-container">
+            {contextHolder}
+            <span className="upload-message">Select the Excel file you want to upload</span>
+            <input className="file-input" type="file" onChange={handleFileChange} />
+            <Button className="upload-button" onClick={handleFileUpload}>Upload</Button>
         </div>
     )
 }
